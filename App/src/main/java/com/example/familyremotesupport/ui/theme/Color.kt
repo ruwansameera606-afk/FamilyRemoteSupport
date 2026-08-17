475d329893324744bcc9daec745754d67d533a3e
ruwansameera606-afk <ruwansameera606@gmail.com>
2026-08-17 08:10:12 +0530

    Create Color.kt

---
+++ App/src/main/java/com/example/familyremotesupport/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.familyremotesupport.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1565C0)
+val Secondary = Color(0xFF42A5F5)
+val Background = Color(0xFFF5F7FA)
+val Surface = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFF1A1A1A)
