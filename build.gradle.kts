daa51e241cb51a241835e92c81acc4b525f87b57
ruwansameera606-afk <ruwansameera606@gmail.com>
2026-08-17 08:27:03 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
+implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
