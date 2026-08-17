0fdd288a47bc0851e649db29ce3da201649f1252
ruwansameera606-afk <ruwansameera606@gmail.com>
2026-08-17 07:47:37 +0530

    Create build.gradle.kts

---
+++ App/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.example.familyremotesupport"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.familyremotesupport"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
